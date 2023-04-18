dfd7f9937604cbb4ec25381cc61cbe4268b04d4f
Chenyi Xiang <xiang.che@northeastern.edu>
2023-04-18 11:33:51 -0400

    Update of Comments

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+# Import custom modules for WordNet and BERT
 import wordnet_module
 from bert_module import similarity
 
+# Define a word to get meanings and parents for
 word = 'paper'
 
+# Get the meanings of the word using WordNet
 meanings = wordnet_module.get_meanings(word)
 print(f"Meanings of '{word}':")
 for meaning in meanings:
     print(f"- {meaning}")
 
+# Get the parents of the word using WordNet
 parents = wordnet_module.get_parent(word)
 print(f"\nParents of '{word}':")
 for parent in parents:
     print(f"- {', '.join(parent)}")
 
+# Define two texts to compare using cosine similarity
 text1 = "software"
 text2 = "program"
 
+# Compute the cosine similarity between the two texts using BERT
 similarity_score = similarity(text1, text2)
-print(f"The similarity between '{text1}' and '{text2}' is: {similarity_score}")
+print(f"The similarity between '{text1}' and '{text2}' is: {similarity_score}") 
--- wordnet_module.py
+++ wordnet_module.py
@@ -1,19 +1,22 @@
+# Import WordNet corpus from NLTK library
 from nltk.corpus import wordnet as wn
 
 
+# Get all meanings of a word
 def get_meanings(word):
-    synsets = wn.synsets(word)
+    synsets = wn.synsets(word)  # Get all Synset objects related to the word
     meanings = []
     for synset in synsets:
-        meanings.append(synset.definition())
+        meanings.append(synset.definition())  # Add the definition of each Synset to the list of meanings
     return meanings
 
 
+# Get all parent synsets of a word
 def get_parent(word):
-    synsets = wn.synsets(word)
+    synsets = wn.synsets(word)  # Get all Synset objects related to the word
     parents = []
     for synset in synsets:
-        hypernyms = synset.hypernyms()
+        hypernyms = synset.hypernyms()  # Get all hypernym Synset objects of the Synset
         for hypernym in hypernyms:
-            parents.append(hypernym.lemma_names())
+            parents.append(hypernym.lemma_names())  # Add the lemma names of each hypernym Synset to the list of parents
     return parents
